27a9a3ec6aea1b1f1b6def47279da2e32947d4eb
RenerJeielBorais <46257262+RenerJeielBorais@users.noreply.github.com>
2022-01-06 20:38:13 +0800

    Update OrderScreen.js
    Heheh hello

--- app/screens/Orders/OrderScreen.js
+++ app/screens/Orders/OrderScreen.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
-import { Text } from "react-native";
+import { Text, StyleSheet, View} from "react-native";
 
 function OrderScreen({ navigation, route }) {
 	const [isAdmin] = useState(route.params.isAdmin);
 
 	return (
+		<View
+		style = {{
+			flex :1,
+			backgroundColor: 'lightblue',
+		}}
+		>
 		<Text style={{ justifyContent: "center", alignSelf: "center" }}>
 			Order Screen
 		</Text>
+		</View>
 	);
 }
 
