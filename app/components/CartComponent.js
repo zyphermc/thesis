6c71c75d68d895a04b34b5d750e3a86626d88229
RenerJeielBorais <boraisrener@gmail.com>
2022-01-11 17:31:30 +0800

    Cart
    cart na pangit

--- app/components/CartComponent.js
+++ app/components/CartComponent.js
@@ -1,55 +1,69 @@
 import React from "react";
-import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity, Button, Alert } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 function CartComponent(props) {
-    const { name, imageURI, quantity, sellingPrice, vat } = props
+    const { name, imageURI, quantity, sellingPrice, vat, } = props
     return (
-        <View
-            style={{
-                flex: 1
-            }}
-        >
+        <View>
             <View
-                style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    marginTop: 10,
-                    marginBottom: 10
-                }}
+                style={styles.container}
             >
                 <Image
                     source={{ uri: props.imageURI }}
-                    style={{
-                        width: 100,
-                        height: 100,
-                        borderRadius: 30,
-                    }}
+                    style={styles.image}
                 />
                 <Text
-                    style={{
-                        marginLeft: 10
-                    }}
-                >{name}</Text>
+                    style={styles.textStyle}
+                >{name}
+                </Text>
                 <Text
                     style={{
-                        marginLeft: 10
+                        marginLeft: 15,
+                        fontWeight: 'bold',
+                        fontSize: 15
                     }}
-                >{quantity}</Text>
+                >{quantity}
+                </Text>
                 <Text
                     style={{
-                        marginLeft: 10
+                        marginLeft: 45,
+                        fontWeight: 'bold',
+                        fontSize: 15
                     }}
                 >{sellingPrice}</Text>
                 <Text
                     style={{
-                        marginLeft: 30
+                        marginLeft: 42,
+                        fontWeight: 'bold',
+                        fontSize: 15
                     }}
-                >
-                    {sellingPrice * quantity}
-                </Text>
+                >{quantity * sellingPrice}</Text>
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 10,
+        marginBottom: 10,
+        borderRadius: 10,
+        borderColor: 'black',
+    },
+    image: {
+        width: 90,
+        height: 90,
+    },
+    textStyle: {
+        width: 75,
+        marginLeft: 10,
+        fontWeight: 'bold',
+        fontSize: 15
+    },
+
+});
 
 export default CartComponent;
--- app/screens/Orders/Cart.js
+++ app/screens/Orders/Cart.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, Button, Image, FlatList } from "react-native";
+import { Text, View, Button, Image, FlatList, TouchableOpacity } from "react-native";
 import CartComponent from "../../components/CartComponent";
 
 
@@ -21,22 +21,86 @@ function Cart(props) {
 
 	return (
 		<View>
-			<FlatList
-				data={orderProductList}
-				keyExtractor={(item, index) => index.toString()}
-				renderItem={({ item }) => (
-					<CartComponent
-						name={item.productName}
-						imageURI={item.imageURI}
-						quantity={item.quantity}
-						sellingPrice={item.sellingPrice}
-						vat={item.vat}
-					/>
-				)}
+			<View>
+				<View
+					style={{
+						width: "100%",
+						height: 30,
+						backgroundColor: 'black',
+						justifyContent: 'center'
+					}}
+				>
+					<Text
+						style={{
+							fontSize: 18,
+							fontWeight: 'bold',
+							color: 'white',
+							marginLeft: 100
+						}}
+					>Product    Qty      Price       Total</Text>
+				</View>
+				<FlatList
+					data={orderProductList}
+					keyExtractor={(item, index) => index.toString()}
+					renderItem={({ item }) => (
+						<CartComponent
+							name={item.productName}
+							imageURI={item.imageURI}
+							quantity={item.quantity}
+							sellingPrice={item.sellingPrice}
+							vat={item.vat}
+						/>
+					)}
 
-			/>
-			<Text>{totalValue}</Text>
+				/>
+			</View>
+			<View style={{ flex: 1 }}>
+				<View>
+					<Text style={{ color: 'black' }}>HELLO WORLD</Text>
+				</View>
+				<View style={{
+					position: 'absolute',
+					left: 0,
+					right: 0,
+					bottom: 0,
+					height: 30,
+					justifyContent: 'center',
+					backgroundColor: 'orange',
+				}}>
+					<Text
+						style={{
+							fontSize: 23,
+							fontWeight: 'bold',
+							alignSelf: 'flex-end',
+							marginRight: 40
+						}}
+					>₱{totalValue}</Text>
+				</View>
+			</View>
+
+			<TouchableOpacity
+				style={{
+					height: 25,
+					width: 100,
+					borderRadius: 20,
+					borderWidth: 2,
+					borderColor: 'black',
+					backgroundColor: 'orange'
+				}}
+				onPress={() => (console.Console)}
+			>
+				<Text
+					style={{
+						justifyContent: 'center',
+						alignItems: 'center',
+						textAlign: 'center',
+						fontWeight: 'bold',
+						fontSize: 15
+					}}
+				>CHECKOUT</Text>
+			</TouchableOpacity>
 		</View>
+
 	);
 }
 
